b19225a72b00cc09638322e742590ac56c9ace2c
Chris Mumford <christopher.mumford@arnoldclark.com>
2022-10-13 20:39:07 +0100

    ✨ Add movie to list from bookmarks

--- src/Movies/MovieBookmarks.js
+++ src/Movies/MovieBookmarks.js
@@ -3,11 +3,13 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { useEffect, useState } from 'react';
 import MovieCard from './MovieCard';
-import { Alert, AlertTitle, Grid } from '@mui/material';
+import { Alert, AlertTitle, Button, Card, CardActions, CardContent, CardMedia, Grid, TextField } from '@mui/material';
 
 function MovieBookmarks(props) {
   const [movieBookmarks, setMovieBookmarks] = useState([]);
   const [movieRefreshKey, setMovieRefreshKey] = useState(0);
+  const [selectedMovie, setSelectedMovie] = useState();
+  const [showAddMovieModal, setShowAddMovieModal] = useState(false);
 
   const style = {
     position: 'absolute',
@@ -21,6 +23,38 @@ function MovieBookmarks(props) {
     boxShadow: 24,
     p: 4,
     overflowY: 'scroll'
+  };
+
+  // TODO: this is duped from AddMovie.js
+  const handleMovieSelected = movie => {
+    setSelectedMovie(movie);
+    toggleShowAddMovieModal(true);
+  };
+
+  // TODO: this is duped from AddMovie.js
+  const cancelMovieSelection = () => {
+    setSelectedMovie(null);
+    toggleShowAddMovieModal(false);
+  };
+
+  // TODO: this is duped from AddMovie.js
+  const addMovieToList = () => {
+    const currentMovieList =
+      JSON.parse(localStorage.getItem('movie-list')) ?? [];
+
+    currentMovieList.push(selectedMovie);
+    localStorage.setItem('movie-list', JSON.stringify(currentMovieList));
+
+    toggleShowAddMovieModal(false);
+  };
+
+  const toggleShowAddMovieModal = show => {
+    setShowAddMovieModal(show);
+  };
+
+  // TODO: this is duped from AddMovie.js
+  const movieDateSelected = date => {
+    setSelectedMovie({ ...selectedMovie, watchDate: date });
   };
 
   function onRemoveBookmark(movie) {
@@ -85,6 +119,7 @@ function MovieBookmarks(props) {
                         key={movie.id}
                         movie={movie}
                         removeBookmark={onRemoveBookmark}
+                        movieSelected={handleMovieSelected}
                       />
                     </Grid>
                   );
@@ -97,6 +132,57 @@ function MovieBookmarks(props) {
               </Alert>
             )}
           </Grid>
+        </Box>
+      </Modal>
+
+      <Modal
+        open={showAddMovieModal}
+        onClose={() => toggleShowAddMovieModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Card sx={{ maxWidth: 345 }}>
+            <Box sx={{ position: 'relative' }}>
+              <CardMedia
+                component="img"
+                sx={{ objectFit: 'contain' }}
+                image={`https://image.tmdb.org/t/p/original${selectedMovie?.backdrop_path}`}
+                alt="doot"
+              />
+            </Box>
+            <CardContent>
+              <Typography gutterBottom component="div" sx={{ pb: 2 }}>
+                When do you plan to watch {selectedMovie?.title}?
+              </Typography>
+              <TextField
+                id="date"
+                label="Date to watch"
+                type="date"
+                InputLabelProps={{
+                  shrink: true
+                }}
+                onChange={e => movieDateSelected(e.target.value)}
+              />
+            </CardContent>
+            <CardActions sx={{ pb: 4 }}>
+              <Button
+                size="small"
+                variant="contained"
+                onClick={addMovieToList}
+                disabled={!selectedMovie?.watchDate}
+              >
+                Confirm
+              </Button>
+              <Button
+                size="small"
+                variant="outlined"
+                onClick={cancelMovieSelection}
+              >
+                Cancel
+              </Button>
+            </CardActions>
+          </Card>
         </Box>
       </Modal>
     </div>
